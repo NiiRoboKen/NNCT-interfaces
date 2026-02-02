8c044e77722e6e43f0d6109dfba00b37ec5de3a5
zen4215 <44k1.373@gmail.com>
2026-02-02 15:52:28 +0000

    feat:motor

--- motor.hpp
+++ motor.hpp
@@ -1,2 +1,20 @@
 #pragma once
 
+#include <inttypes.h>
+
+namespace NNCT {
+namespace interfaces {
+    
+class Motor {
+    public:
+    virtual void cw (uint8_t speed) = 0;
+    virtual void ccw(uint8_t speed) = 0;
+    
+    void run(int16_t speed) {
+        if(speed > 0) this->cw(speed);
+        else          this->ccw(speed);
+    }
+};
+
+}
+}
