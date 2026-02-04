c71f37d694ac7ff660e7b3decd32dbea32bdc6ee
zen4215 <44k1.373@gmail.com>
2026-02-04 12:58:31 +0000

    rm:readable motor

--- readable_motor.hpp
+++ readable_motor.hpp
@@ -7,14 +7,6 @@ namespace interfaces {
 
 class ReadableMotor : public Motor {
     public:
-    virtual void cw (float rpm) = 0;
-    virtual void ccw(float rpm) = 0;
-    
-    void run(float rpm) {
-        if(rpm > 0) this->cw(rpm);
-        else        this->ccw(-rpm);
-    }
-    
     virtual float getRPM() = 0;
 };
 
