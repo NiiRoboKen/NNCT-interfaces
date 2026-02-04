5dc78a05a40800c4f2c29555c311452bcf308adf
zen4215 <44k1.373@gmail.com>
2026-02-04 11:21:54 +0000

    feat:servo

--- servo.hpp
+++ servo.hpp
@@ -1,2 +1,16 @@
 #pragma once
 
+namespace NNCT {
+namespace interfaces {
+
+class Servo {
+    public:
+    virtual void setAngle(float degree) = 0;
+    
+    protected:
+    const float MAX_RANGE;
+    const float MIN_RANGE;
+};
+
+}
+}
