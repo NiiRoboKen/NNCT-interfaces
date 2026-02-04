e8d9dffcf189246f97fd4f24c6ff4ae682ad2150
zen4215 <44k1.373@gmail.com>
2026-02-04 11:32:52 +0000

    fix:motor

--- motor.hpp
+++ motor.hpp
@@ -7,12 +7,12 @@ namespace interfaces {
     
 class Motor {
     public:
-    virtual void cw (uint8_t speed) = 0;
-    virtual void ccw(uint8_t speed) = 0;
+    virtual void cwDuty (uint8_t speed) = 0;
+    virtual void ccwDuty(uint8_t speed) = 0;
     
-    void run(int16_t speed) {
-        if(speed > 0) this->cw(speed);
-        else          this->ccw(speed);
+    void runDuty(int16_t speed) {
+        if(speed > 0) this->cwDuty(speed);
+        else          this->ccwDuty(speed);
     }
 };
 
